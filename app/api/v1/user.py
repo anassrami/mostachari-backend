a9c5c1876d9a8d7c0dbb4825aa2f20691b2d95a1
chrorou <yassin.chrorou@gmail.com>
2024-06-29 16:44:16 +0100

    add change user email

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from pymongo.collection import Collection
 
 from app.dependencies import get_database
-from app.schemas.user import ChangeRole, UserDetails
-from app.services.user_service import user_change_role
+from app.schemas.user import ChangeMail, ChangeRole, UserDetails
+from app.services.user_service import user_change_mail, user_change_role
 from app.utils.auth_utils import get_current_user
 
 router = APIRouter()
@@ -20,3 +20,9 @@ def read_user(roleObject: ChangeRole ,user = Depends(get_current_user), db: Coll
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user_change_role(user,  db , roleObject.role)
+
+@router.put("/email", response_model=UserDetails )
+def read_user(mailObject: ChangeMail ,user = Depends(get_current_user), db: Collection = Depends(get_database)):
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user_change_mail(user,  db , mailObject.email)
